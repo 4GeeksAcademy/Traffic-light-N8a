005ce05dd7308cac223876041bfd9530efece5e8
Natalie8a <naty.8a@outlook.com>
2025-04-29 00:37:21 +0000

    all main things done, couple css issues to fix

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [color, setColor] = useState("red");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	useEffect(() => {
+		const colors = ["red", "yellow", "green"];
+		let index = 0;
+
+		const interval = setInterval(() => {
+			index = (index + 1) % colors.length; // alternates between 0, 1, 2
+			setColor(colors[index]);
+		}, 3000); // every 3 secs
+	
+		return () => clearInterval(interval); // clears interval to unmount
+	}, []);
+
+	return (
+		<>
+		<div className="v-line"></div>
+		<div className="h-line"></div>
+		<div className="outerPart">
+		<div className="traffic-light-container">
+            <div 
+			className={`light red ${color === "red" ? "selected" : ""}`}
+			onClick={() => setColor("red")}></div>
+
+				<div
+				className= {`light yellow ${color === "yellow" ? "selected" : ""}`}
+				onClick={() => setColor("yellow")}></div>
+				<div 
+				className= {`light green ${color === "green" ? "selected" : ""}`}
+						onClick={() => setColor("green")}></div>
+
+						
+				
+		</div></div>
+		</>
 	);
+
 };
 
 export default Home;
